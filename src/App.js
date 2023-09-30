c88e9f784d0612914d521d238ae04618937691c3
ManmohanJangra25 <manmohanjangra2528@gmail.com>
2023-09-30 23:55:16 +0530

    Create Custom Hooks And How To Use Them

--- src/App.js
+++ src/App.js
@@ -1,43 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useHTTP from './components/Hooks/use-http';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
-
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
+  
+  const { isLoading, error, sendRequest: fetchTasks } = useHTTP();
+  useEffect(() => {
+    const transformTasks = tarsksObj => {
       const loadedTasks = [];
 
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      for (const taskKey in tarsksObj) {
+        loadedTasks.push({ id: taskKey, text: tarsksObj[taskKey].text });
       }
 
       setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    fetchTasks();
-  }, []);
+    };
+    fetchTasks({url: 'https://react-backend-26d42-default-rtdb.firebaseio.com/tasks.json'}, transformTasks);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
---
+++ src/components/Hooks/use-http.js
@@ -0,0 +1,37 @@
+import  {useCallback, useState} from "react";
+
+const useHTTP = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const sendRequest = useCallback( async (requestConfig, applyData) => {
+        setIsLoading(true);
+        setError(null);
+        try {
+            const response = await fetch(
+                requestConfig.url, {
+                    method: requestConfig.method ? requestConfig.method : 'GET',
+                    headers: requestConfig.headers ? requestConfig.headers : {},
+                    body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+                }
+            );
+
+            if (!response.ok) {
+                throw new Error('Request failed!');
+            }
+
+            const data = await response.json();
+            applyData(data); //Passes all data inside the function.
+        } catch (err) {
+            setError(err.message || 'Something went wrong!');
+        }
+        setIsLoading(false);
+    }, []);
+
+    return {
+        isLoading: isLoading,
+        error: error,
+        sendRequest: sendRequest, //Passes a function
+    };
+};
+
+export default useHTTP;
